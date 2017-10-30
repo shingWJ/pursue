320bc7f6e21cac6bd7ec5901b572ad1dfcfbe58d
unknown <812990275@163.com>
2017-10-30 17:50:55 +0800

    db

--- settings.js
+++ settings.js
@@ -1,6 +1,12 @@
+// module.exports = {
+// 	cookieSecret: 'pursue',
+// 	db: 'pursue',
+// 	host: 'localhost',
+// 	port: 27017
+// };
 module.exports = {
 	cookieSecret: 'pursue',
-	db: 'pursue',
-	host: 'localhost',
-	port: 27017
+	db: 'duckweed',
+	host: 'root:123456@ds137340.mlab.com',
+	port: 37340
 };
