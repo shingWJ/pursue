d56949d37afd0b8ad767230f42259955325e4626
unknown <812990275@163.com>
2017-10-31 11:20:07 +0800

    Signed-off-by: unknown <812990275@163.com>

--- model/db.js
+++ model/db.js
@@ -1,6 +1,16 @@
 var settings = require('../settings');
 var Db = require('mongodb').Db;
 var Connection = require('mongodb').Connection;
-var Server = require('mongodb').Server;
+var Server = require('mongodb').Server,
+	MongoClient=require('mongodb').MongoClient;
 
-module.exports = new Db(settings.db, new Server(settings.host,settings.port),{safe:true});
+// module.exports = new Db(settings.db, new Server(settings.host,settings.port),{safe:true});
+
+module.exports = new MongoClient(new Server(settings.host,settings.port,{
+                             socketOptions:{connectTimeoutMS:500},
+                              poolSize:5,
+                              auto_reconnect:true
+                          },{
+                              numberOfRetries:3,
+                            retryMiliSeconds:500
+                         })).db(settings.db);
